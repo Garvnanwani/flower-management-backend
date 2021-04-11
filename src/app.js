32448b9cbdce167fb4b7c280a1e296cf44bb0183
Garvnanwani <garvnanwani88@gmail.com>
2021-04-11 15:59:27 +0530

    applied essential middlewares

--- src/app.js
+++ src/app.js
@@ -1,19 +1,30 @@
 const express = require('express')
 require("dotenv").config();
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+// Import Routes
 const productsRouter = require('./routes/products');
 const testRouter = require('./routes/testRoute');
 
 const app = express()
 
-app.use(express.json())
+// Middlewares
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
+// Routes
 app.use('/products', productsRouter);
 app.use('/', testRouter);
 
+
+// Run the server
 const PORT = process.env.PORT || 5000
-
-
 
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`);
