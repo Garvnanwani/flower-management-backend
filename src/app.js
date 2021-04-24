16a25bd09d8773abf66d3314f79a1815b85cbc59
farihasalman <farihasalman01@gmail.com>
2021-04-24 19:10:12 +0530

    init

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+const express = require('express');
 require("dotenv").config();
 const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
