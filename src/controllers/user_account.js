ab84c0b606ae43ea33f22b0e8293f826869b7c58
nishtha <81146172+Nishthaguptaa@users.noreply.github.com>
2021-04-27 03:40:35 +0530

    Create user_account.js

---
+++ src/controllers/user_account.js
@@ -0,0 +1,12 @@
+export const handleMyAcc=(req,res,db)=>{
+    const {user_id}=req.body;
+
+    const sql=`select * from user where user_id=${user_id}`;
+
+    db.query(sql,(err,users)=>{
+        if(err) {res.json("no such user exist"); return ;}
+        res.json(users[0]);
+        // console.log(user)
+    })
+
+}
