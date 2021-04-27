45fb60897d2fb323417eb9178ac213c7fad21267
Garv <garvwashere@gmail.com>
2021-04-27 14:24:22 +0530

    used async await in categories controller

--- src/routes/testRoute.js
+++ src/routes/testRoute.js
@@ -1,25 +1,18 @@
-const express = require('express');
-const router = express.Router();
-const connect = require('../config/db');
+const express = require('express')
+const router = express.Router()
+const connect = require('../config/db')
 
 router.get('/', async (req, res) => {
-
     try {
-
         const con = await connect()
 
-        const sql = "SELECT * FROM users"
+        const sql = 'SELECT * FROM users'
 
-        const data = await con.query(sql)
+        const [rows, fields] = await con.query(sql)
 
-        console.log("working");
-
-        res.json(data[0][0])
-
+        res.json(rows)
     } catch (err) {
-
-        console.log(err);
-
+        console.log(err)
     }
 })
 
