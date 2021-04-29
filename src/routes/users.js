e9ee660bc341c4bcaee5e051603c823f609f5158
Garv <garvwashere@gmail.com>
2021-04-29 18:22:54 +0530

    removed unused routes for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,21 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const {
-    getAllUser,
     getSingleUser,
-    postAddUser,
     postEditUser,
-    getDeleteUser,
     changePassword,
+    // getAllUser,
+    // postAddUser,
+    // getDeleteUser,
 } = require('../controller/users')
 
-router.get('/all-user', getAllUser)
 router.post('/signle-user', getSingleUser)
-
-router.post('/add-user', postAddUser)
 router.post('/edit-user', postEditUser)
-router.post('/delete-user', getDeleteUser)
-
 router.post('/change-password', changePassword)
+
+// router.get('/all-user', getAllUser)
+// router.post('/add-user', postAddUser)
+// router.post('/delete-user', getDeleteUser)
 
 module.exports = router
