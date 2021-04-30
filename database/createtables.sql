7e3f1b33cca88e100692a66ade67cbec2dd1f077
Garv <garvwashere@gmail.com>
2021-04-30 15:05:29 +0530

    removed NOT NULL

--- database/createtables.sql
+++ database/createtables.sql
@@ -1,17 +1,17 @@
 --Create user table
 CREATE TABLE users(
-  user_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(50) NOT NULL,
-  email VARCHAR(50) NOT NULL,
-  password VARCHAR(200) NOT NULL,
-  default_shipping_address VARCHAR(100) NOT NULL,
-  phone_number INT NOT NULL,
+  user_id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50),
+  email VARCHAR(50),
+  password VARCHAR(200),
+  default_shipping_address VARCHAR(100),
+  phone_number INT,
   user_role TINYINT(1) DEFAULT 0,
   register_date DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 -- Create product table
 CREATE TABLE products(
-  product_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  product_id INT AUTO_INCREMENT PRIMARY KEY,
   pName VARCHAR(50),
   pDescription VARCHAR(300),
   pPrice INT,
@@ -25,15 +25,15 @@ CREATE TABLE products(
 -- Create product_category table
 CREATE TABLE categories(
   category_id INT AUTO_INCREMENT PRIMARY KEY,
-  cName VARCHAR(30) NOT NULL,
+  cName VARCHAR(30),
   cDescription VARCHAR(250),
   cImage VARCHAR(100),
   cStatus VARCHAR(20)
 );
 -- Create orders table
 CREATE TABLE orders (
-  order_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  total_amount INT NOT NULL DEFAULT '0.00',
+  order_id INT AUTO_INCREMENT PRIMARY KEY,
+  total_amount INT DEFAULT '0.00',
   created_on DATETIME DEFAULT CURRENT_TIMESTAMP,
   status VARCHAR(50) DEFAULT "Not processed",
   user_id INT,
@@ -43,13 +43,13 @@ CREATE TABLE orders (
 );
 -- Create order_details table
 CREATE TABLE order_details (
-  item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  order_id INT NOT NULL,
-  product_id VARCHAR(10) NOT NULL,
-  product_name VARCHAR(100) NOT NULL,
+  item_id INT AUTO_INCREMENT PRIMARY KEY,
+  order_id INT,
+  product_id VARCHAR(10),
+  product_name VARCHAR(100),
   product_cost INT,
-  quantity INT NOT NULL,
-  payment_mode VARCHAR(50) NOT NULL
+  quantity INT,
+  payment_mode VARCHAR(50)
 );
 CREATE TABLE reviews(
   review_id INT AUTO_INCREMENT PRIMARY KEY,
